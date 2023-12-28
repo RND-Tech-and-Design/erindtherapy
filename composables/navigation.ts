d97ead051a6867915709a11c7f2a4b7bf7be65c3
RyanDierickx <ryan.dierickx@gmail.com>
2023-12-27 22:22:51 -0800

    Refactor navigation composable and add Seattle Couples Therapy page

--- composables/navigation.ts
+++ composables/navigation.ts
@@ -10,107 +10,99 @@ export type NavLink = {
     active?: boolean;
     order?: number;
     children?: NavLink[];
+    noNav?: boolean;
 };
 
 const getCapitalizedRouteName = (route: RouteRecordNormalized) => {
     const name = route.meta?.title || (route.name === 'index' ? 'home' : route.name);
-    if (typeof name === 'string') {
-        return name
-            .toLowerCase()
-            .replace(/\b\w/g, char => char.toUpperCase());
+    return typeof name === 'string' ? name.toLowerCase().replace(/\b\w/g, char => char.toUpperCase()) : '';
+};
+``
+const getNavLink = (route: RouteRecordNormalized, currentPath: string): NavLink => {
+    return {
+        path: route.path,
+        active: currentPath === route.path,
+        name: getCapitalizedRouteName(route),
+        order: route.meta?.navOrder as number,
+        noNav: route.meta?.noNav as boolean ?? false
+    };
+};
+
+const sortNavLinks = (a: NavLink, b: NavLink): number => {
+    const aIndex = a.order ?? Infinity;
+    const bIndex = b.order ?? Infinity;
+    return aIndex - bIndex || a.name.localeCompare(b.name);
+};
+
+const createGroupName = (pathPart: string): string => startCase(toLower(pathPart));
+
+const createChildPagePath = (pathParts: string[]): string => `/${pathParts.join('/').toLowerCase()}`;
+
+const addGroupIfNotExists = (groups: Map<string, NavLink>, groupName: string, lowercasedGroupName: string) => {
+    if (!groups.has(groupName)) {
+        groups.set(groupName, {
+            name: groupName,
+            path: `/${lowercasedGroupName}`,
+            children: []
+        });
     }
-    return '';
-}
+    return groups.get(groupName);
+};
+
+const addChildPageToGroup = (parentPage: NavLink, page: NavLink, pageName: string, childPagePath: string) => {
+    parentPage.children = parentPage.children || []; // Initialize parentPage.children as an empty array if it is undefined
+    parentPage.children.push({ ...page, name: pageName, path: childPagePath });
+};
+
+const handleGrouping = (groups: Map<string, NavLink>, page: NavLink, pathParts: string[]) => {
+    const groupName = createGroupName(pathParts[0]);
+    const lowercasedGroupName = groupName.toLowerCase();
+
+    if (lowercasedGroupName === 'pages') return;
+
+    let parentPage = addGroupIfNotExists(groups, groupName, lowercasedGroupName);
+    const childPagePath = createChildPagePath(pathParts);
+    const pageName = page.name ?? createGroupName(pathParts[1]);
+
+    addChildPageToGroup(parentPage!, page, pageName, childPagePath);
+};
+
+const groupPages = (routes: RouteRecordNormalized[], currentPath: string): NavLink[] => {
+    const groups = new Map<string, NavLink>();
+
+    routes.forEach(route => {
+        const page = getNavLink(route, currentPath);
+        if (!page.path) return;
+        if (page.noNav) return;
+
+        const pathParts = page.path.split('/').filter(Boolean);
+        pathParts.length > 1 ? handleGrouping(groups, page, pathParts) : groups.set(page.name, page);
+    });
+
+    return Array.from(groups.values());
+};
+
+
 
 export const useNavigation = () => {
-
-    // Create a state to cache the navigation links
-    const navigationState = useState<{ name: string; path: string; }[]>('navigation-links', () => []);
+    const navigationState = useState<NavLink[]>('navigation-links', () => []);
+    const router = useRouter();
 
     const generateNavigation = async (): Promise<NavLink[]> => {
-
-        const router = useRouter()
-        // Use cached navigation links if available
+        // If the cached navigation state is not empty, return it
         if (!isEmpty(navigationState.value)) {
-            return navigationState.value.map(page => ({
-                ...page,
-                active: router.currentRoute.value?.path === page.path
-            }));
+            return navigationState.value.map(page => ({ ...page, active: router.currentRoute.value?.path === page.path }));
         }
 
-        const pages = router.getRoutes()
-            .map(route => ({
-                path: route.path,
-                active: router.currentRoute.value?.path === route.path,
-                name: getCapitalizedRouteName(route),
-                order: route.meta?.navOrder as number,
-            }))
-            .reduce((groups, page) => {
-                // Skip creating URL object if the path is not present
-                if (!page.path) return groups;
+        const pages = groupPages(router.getRoutes(), router.currentRoute.value?.path);
 
-                const pathParts = page.path.split('/').filter(Boolean); // removes any empty strings from the array
-
-                // Handle pages that belong to a group
-                if (pathParts.length > 1) {
-                    const groupName = startCase(toLower(pathParts[0]));
-                    const pageName = page.name ?? startCase(toLower(pathParts[1]));
-
-                    // Use an object as a map for efficient lookup
-                    let parentPage = groups.map[groupName];
-
-                    const lowercasedGroupName = groupName.toLowerCase();
-
-                    // Create a new group if it doesn't exist and the name is not 'pages'
-                    if (!parentPage && lowercasedGroupName !== 'pages') {
-                        parentPage = {
-                            name: groupName,
-                            path: `/${lowercasedGroupName}`,
-                            children: [],
-                        };
-                        groups.map[groupName] = parentPage;
-                        groups.array.push(parentPage);
-                    }
-
-                    // Add the page to the group's children
-                    if (parentPage) {
-                        const [firstPart, secondPart] = pathParts;
-                        parentPage.children = parentPage.children || [];
-                        parentPage.children.push({
-                            name: pageName,
-                            path: `/${firstPart.toLowerCase()}/${secondPart}`,
-                            order: page.order,
-                        });
-                    }
-                } else {
-                    groups.array.push({
-                        name: page.name,
-                        path: page.path,
-                        order: page.order,
-                    });
-                }
-
-                return groups;
-            }, { map: {}, array: [] } as { map: Record<string, NavLink>, array: NavLink[] })
-            .array;
-
-
-        // Sort the pages in each group
-        const sortNavLinks = (a: NavLink, b: NavLink): number => {
-            const aIndex = a.order ?? Infinity;
-            const bIndex = b.order ?? Infinity;
-            const orderComparison = bIndex - aIndex;
-            return orderComparison !== 0 ? orderComparison : a.name.localeCompare(b.name);
-        }
-
-        for (const page of pages) {
+        // Sort pages and their children if any
+        pages.forEach(page => {
             if (page.children) {
-                page.children.sort(sortNavLinks).reverse();
+                page.children.sort(sortNavLinks);
             }
-        }
-
-        // Sort the top-level pages
-        pages.sort(sortNavLinks).reverse();
+        });
+        pages.sort(sortNavLinks);
 
         navigationState.value = pages;
         return pages;
---
+++ pages/services/seattleCouplesTheapy.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'default',
+    title: 'Seattle Couples Therapy',
+    noNav: true,
+    description: `Seattle Based Couples Therapy. Enhance your relationship with Couples Therapy: Embrace Gottman Method techniques for 
+    lasting emotional connection, effective communication, and conflict resolution with a Level 3 Gottman-certified therapist.`
+})
+</script>
+
+<template>
+    <Hero headline="Seattle Couples Therapy" hero-image="/images/banner/about.webp" />
+
+    <section>
+        <p>This page will be displayed at the /services/couplesTherapy route.</p>
+        It is hidden from the nav
+    </section>
+</template>
