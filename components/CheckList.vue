f5c68b557241b442bf4b4c47d6bb4b7fe5f82b0a
PhoenixMP <112582143+PhoenixMP@users.noreply.github.com>
2024-01-17 03:07:03 +0000

    most content on page, added formatting

---
+++ components/CheckList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+
+interface ListObject {
+    text: string[];
+    style: string;
+}
+interface CheckMarkObject {
+    iconColor: string;
+    iconName: string;
+    iconSize: string;
+}
+
+const props = defineProps<{
+    heading: string;
+    checkMark: CheckMarkObject;
+    list: ListObject;
+}>();
+
+
+</script>
+
+<template>
+    <section class="flex flex-col mt-5">
+
+        <div class="mt-5 lg:text-4xl text-3xl font-medium">
+            <h3>{{ props.heading }}</h3>
+        </div>
+        <ul>
+            <li v-for="(item, index) in   props.list.text  " :key="index"
+                class="mt-7 text-text_primary bg-clip-text font-sans text-base leading-relaxed antialiased">
+                <span :class="props.checkMark.iconColor">
+                    <Icon :name="props.checkMark.iconName" :size="props.checkMark.iconSize" />
+                </span> {{ item }}
+            </li>
+        </ul>
+    </section>
+</template>
+
+
+
