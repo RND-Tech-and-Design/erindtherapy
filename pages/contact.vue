eab9b157ba91828d4227d66c86ff87d7a2e098c0
RyanDierickx <ryan.dierickx@gmail.com>
2024-06-16 22:36:53 -0700

    chore: Update contact form attributes for Netlify integration

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,8 @@
 import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/solid';
 import { ref, watch } from 'vue'
 
+const route = useRoute()
+const success = ref(route.query.success === 'true')
 const email = ref('')
 const name = ref('')
 const service = ref('')
@@ -68,93 +70,109 @@ definePageMeta({
                 </div>
             </div>
             <div class="mt-10">
-                <CardGlass title="Have a Question?" animation-delay="0.8s">
+                <CardGlass :title="success ? '' : 'Have a Question?'" animation-delay="0.8s">
                     <template #content>
-                        <p>
-                            Send me a message with this secure form
-                        </p>
-
-                        <form name="contact"
-                              method="POST"
-                              netlify
-                              data-netlify-recaptcha="true"
-                              netlify-honeypot="bot-field"
-                              class="space-y-4">
-                            <input type="hidden" name="form-name" value="contact" />
-                            <p class="hidden">
-                                <label>
-                                    Whats up with stuff? <input name="bot-field" />
-                                </label>
+                        <div v-if="!success">
+                            <p>
+                                Send me a message with this secure form
                             </p>
-                            <label class="input input-bordered flex items-center gap-2">
-                                <Icon name="line-md:person"></Icon>
-                                <input type="text"
-                                       class="grow bg-transparent"
-                                       placeholder="Your Name"
-                                       name="name"
-                                       v-model="name" />
-                                <span v-if="name === ''" class="badge badge-warning">Required</span>
-                            </label>
+
+                            <form
+                                  name="contact"
+                                  method="POST"
+                                  action="/contact?success=true"
+                                  netlify
+                                  data-netlify-recaptcha="true"
+                                  netlify-honeypot="bot-field"
+                                  class="space-y-4">
+                                <input type="hidden" name="form-name" value="contact" />
+                                <p class="hidden">
+                                    <label>
+                                        Whats up with stuff? <input name="bot-field" />
+                                    </label>
+                                </p>
+                                <label class="input input-bordered flex items-center gap-2">
+                                    <Icon name="line-md:person"></Icon>
+                                    <input type="text"
+                                           class="grow bg-transparent"
+                                           placeholder="Your Name"
+                                           name="name"
+                                           v-model="name" />
+                                    <span v-if="name === ''" class="badge badge-warning">Required</span>
+                                </label>
 
 
-                            <label class="input input-bordered flex items-center gap-2">
-                                <Icon name="line-md:email-twotone"></Icon>
-                                <input type="text"
-                                       class="grow bg-transparent"
-                                       placeholder="Your Email"
-                                       name="email"
-                                       v-model="email" />
-                                <span v-if="email === ''" class="badge badge-warning">Required</span>
-                                <span v-if="invalidEmail" class="badge badge-error">Invalid Email</span>
+                                <label class="input input-bordered flex items-center gap-2">
+                                    <Icon name="line-md:email-twotone"></Icon>
+                                    <input type="text"
+                                           class="grow bg-transparent"
+                                           placeholder="Your Email"
+                                           name="email"
+                                           v-model="email" />
+                                    <span v-if="email === ''" class="badge badge-warning">Required</span>
+                                    <span v-if="invalidEmail" class="badge badge-error">Invalid Email</span>
 
-                            </label>
+                                </label>
 
-                            <label class="form-control">
-                                <div class="label">
-                                    <span class="label-text">What service are you interested in?</span>
-                                </div>
-                                <select name="service"
-                                        v-model="service"
-                                        class="form-select w-full max-w-full input input-bordered flex items-center gap-2">
-                                    <option disabled selected value="">Select a service</option>
-                                    <option value="individual">Individual Therapy</option>
-                                    <option value="couples">Couples Therapy</option>
-                                    <option value="family">Family Therapy</option>
-                                    <option value="family">Other</option>
-                                </select>
-                            </label>
+                                <label class="form-control">
+                                    <div class="label">
+                                        <span class="label-text">What service are you interested in?</span>
+                                    </div>
+                                    <select name="service"
+                                            v-model="service"
+                                            class="form-select w-full max-w-full input input-bordered flex items-center gap-2">
+                                        <option disabled selected value="">Select a service</option>
+                                        <option value="individual">Individual Therapy</option>
+                                        <option value="couples">Couples Therapy</option>
+                                        <option value="family">Family Therapy</option>
+                                        <option value="family">Other</option>
+                                    </select>
+                                </label>
 
-                            <label class="form-control">
-                                <div class="label">
-                                    <span class="label-text">Your Message</span>
-                                </div>
-                                <textarea name="message"
-                                          class="textarea textarea-bordered w-full max-w-full h-32"
-                                          placeholder="Please do not share any confidential or sensitive medical information"></textarea>
-                            </label>
+                                <label class="form-control">
+                                    <div class="label">
+                                        <span class="label-text">Your Message</span>
+                                    </div>
+                                    <textarea name="message"
+                                              class="textarea textarea-bordered w-full max-w-full h-32"
+                                              placeholder="Please do not share any confidential or sensitive medical information"></textarea>
+                                </label>
 
-                            <div data-netlify-recaptcha="true"></div>
+                                <div data-netlify-recaptcha="true"></div>
 
-                            <div class="flex justify-end md:w-auto w-full">
-                                <button type="submit"
-                                        :disabled="!formIsValid"
-                                        class="btn btn-primary text-text_secondary flex items-center md:w-auto w-full md:text-sm text-base">
-                                    <span>
-                                        <span v-if="formIsValid" class="mr-2 text-lg">
-                                            <Icon name="line-md:confirm-circle"></Icon>
+                                <div class="flex justify-end md:w-auto w-full">
+                                    <button type="submit"
+                                            :disabled="!formIsValid"
+                                            class="btn btn-primary text-text_secondary flex items-center md:w-auto w-full md:text-sm text-base">
+                                        <span>
+                                            <span v-if="formIsValid" class="mr-2 text-lg">
+                                                <Icon name="line-md:confirm-circle"></Icon>
+                                            </span>
+                                            <span v-else class="mr-2 text-lg">
+                                                <Icon name="line-md:alert-circle-loop"></Icon>
+                                            </span>
+                                            Submit
                                         </span>
-                                        <span v-else class="mr-2 text-lg">
-                                            <Icon name="line-md:alert-circle-loop"></Icon>
-                                        </span>
-                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div v-else class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4" role="alert">
+                            <div class="flex">
+                                <div class="py-1">
+                                    <span class="mr-2 text-4xl">
+                                        <Icon name="line-md:emoji-smile"></Icon>
                                     </span>
-                                </button>
+                                </div>
+                                <div>
+                                    <p class="font-bold">Thank you for reaching out!</p>
+                                    <p class="text-sm">I will get back to you as soon as possible.</p>
+                                </div>
                             </div>
-                        </form>
+                        </div>
                     </template>
 
                     <template #action>
-
                     </template>
                 </CardGlass>
             </div>
