ad990cda2d53135c73aa52ff75ad14a35771461f
RyanDrx <Ryan.Dierickx@gmail.com>
2024-09-11 04:42:45 +0000

    refactor: Improve About page layout and styling

--- pages/about.vue
+++ pages/about.vue
@@ -106,33 +106,32 @@ const pageData = {
 </script>
 
 <template>
-  <Hero headline="About" hero-image="/images/banner/about.webp" overlayClass="bg-complementary_accent" />
-  <div class="flex flex-col align-center">
+    <Hero headline="About" hero-image="/images/banner/about.webp" overlayClass="bg-complementary_accent" />
+    <div class="flex flex-col align-center">
 
-    <section :class="pageStyle.margins">
-      <AboutMePageIntro v-bind="pageData.aboutMe" />
-    </section>
+        <section :class="pageStyle.margins">
+            <AboutMePageIntro v-bind="pageData.aboutMe" />
+        </section>
 
-    <section :class="pageStyle.backgroundColor">
-      <AboutMePageSection v-bind="pageData.background" :class="pageStyle.margins" />
-    </section>
+        <section :class="pageStyle.backgroundColor">
+            <AboutMePageSection v-bind="pageData.background" :class="pageStyle.margins" />
+        </section>
 
-    <section>
-      <AboutMePageSection v-bind="pageData.qualifications" :class="pageStyle.margins" />
-      <div class=" sm:flex-row md:flex mb-20 sm:mb-20" :class="pageStyle.margins">
-        <div v-for="(qualification, index) in pageData.qualifications.qualifications" :key="index" class="mb-10 md:mb-0">
-          <AboutMePageQualification v-bind="qualification" />
-        </div>
-      </div>
-    </section>
+        <section>
+            <AboutMePageSection v-bind="pageData.qualifications" :class="pageStyle.margins" />
+            <div class="sm:flex-row md:flex mb-20 sm:mb-20" :class="pageStyle.margins">
+                <div v-for="(qualification, index) in pageData.qualifications.qualifications" :key="index"
+                     class="flex-1 mb-10 md:mb-0">
+                    <AboutMePageQualification v-bind="qualification" />
+                </div>
+            </div>
+        </section>
 
-    <section :class="pageStyle.backgroundColor">
- <AboutMePageSection v-bind="pageData.homeAndHobbies" :class="pageStyle.margins" />
-    </section>
-  </div>
 
-  <Cta></Cta>
+        <section :class="pageStyle.backgroundColor">
+            <AboutMePageSection v-bind="pageData.homeAndHobbies" :class="pageStyle.margins" />
+        </section>
+    </div>
+
+    <Cta></Cta>
 </template>
-
-
-
