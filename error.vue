2dcd087990a86535f06fa38a990a0daebad2b12b
RyanDrx <Ryan.Dierickx@gmail.com>
2024-11-15 03:28:47 +0000

    add error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup>
+const error = useError();
+</script>
+
+
+<template>
+    <NuxtLayout>
+        <AppHeader />
+        <div itemscope itemtype="https://schema.org/Psychiatric">
+            <h1 v-if="error.statusCode === 404">Oh No! 404 - Page Not Found!</h1>
+            <h1 v-else>Something went wrong ({{ error.statusCode }})</h1>
+            <p>{{ error.message }}</p>
+            <NuxtLink to="/" class="back-home">Go back home</NuxtLink>
+        </div>
+        <AppFooter />
+    </NuxtLayout>
+</template>
