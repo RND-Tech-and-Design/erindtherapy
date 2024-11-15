725f54e17c7206acd7ac100bc9a29527e87188c0
RyanDrx <Ryan.Dierickx@gmail.com>
2024-11-15 21:55:20 +0000

    ...

--- error.vue
+++ error.vue
@@ -5,13 +5,13 @@ const error = useError();
 
 <template>
     <NuxtLayout>
-        <AppHeader />
+ 
         <div itemscope itemtype="https://schema.org/Psychiatric">
             <h1 v-if="error.statusCode === 404">Oh No! 404 - Page Not Found!</h1>
             <h1 v-else>Something went wrong ({{ error.statusCode }})</h1>
             <p>{{ error.message }}</p>
             <NuxtLink to="/" class="back-home">Go back home</NuxtLink>
         </div>
-        <AppFooter />
+        
     </NuxtLayout>
 </template>
--- middleware/routeGuard.global.ts
+++ middleware/routeGuard.global.ts
@@ -49,8 +49,17 @@ export default defineNuxtRouteMiddleware((to) => {
         ['/blog/5-love-languages', '/blog/the-ways-we-love'],
     ]);
 
-    const redirect =
-        redirects.get(to.path) || redirects.get(to.path + '/' );
+
+    
+    const originalPath = to.path;
+    const lastSlashIndex = originalPath.lastIndexOf('/');
+
+    // Replace the last occurrence of '/' if it exists
+    const newPath = lastSlashIndex !== -1
+        ? originalPath.substring(0, lastSlashIndex) + originalPath.substring(lastSlashIndex + 1)
+        : originalPath;
+
+    const redirect = redirects.get(newPath) || redirects.get(originalPath) ;
 
     if (redirect) {
         return navigateTo(redirect, { redirectCode: 301 });
