343d17312326485095b75fbb9ce50e472c20020c
RyanDrx <Ryan.Dierickx@gmail.com>
2024-12-05 00:39:41 +0000

    add Couples Counseling Seattle page

---
+++ pages/services/couplesCounselingSeattle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'default',
+    title: 'Couples Counseling Seattle',
+    noNav: true,
+    description: `Couples Counseling in Seattle. Enhance your relationship with Couples Therapy: Embrace Gottman Method techniques for 
+    lasting emotional connection, effective communication, and conflict resolution with a Level 3 Gottman-certified therapist.`
+})
+</script>
+
+<template>
+    <CouplesTherapyCopy pageTitle="Couples Counseling Seattle"> </CouplesTherapyCopy>
+</template>
